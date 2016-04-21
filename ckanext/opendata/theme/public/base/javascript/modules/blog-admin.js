2638bd3c33c2e37c221960bb83e068ce38f65790
root <root@giv-oct.uni-muenster.de>
2016-04-21 19:33:27 +0200

    first commit

--- ckanext/opendata/theme/public/base/javascript/modules/blog-admin.js
+++
@@ -1,9 +0,0 @@
-this.ckan.module('blog-admin', function (jQuery) {
-  return {
-    initialize: function () {
-      jQuery('.remove-link').click(function () {
-        console.log('Hej');
-      });
-    }
-  };
-});
